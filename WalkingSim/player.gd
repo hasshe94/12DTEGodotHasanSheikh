1817c03fe40956eed2176d3956d22a18e12b9387
hasshe94 <hasshe94@kingshigh.school.nz>
2023-06-26 10:46:35 +1200

    Designed the Level
    Need to improve the level and adjust it to make it a suitable size

--- WalkingSim/player.gd
+++ WalkingSim/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 @export var mouse_sensitivity = 0.001
-const SPEED = 5.0
+const SPEED = 10.0
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
