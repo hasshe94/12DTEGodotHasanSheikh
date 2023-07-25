6921df8c92c793689e554b0e408eece0d2357bfe
hasshe94 <hasshe94@kingshigh.school.nz>
2023-07-25 18:03:38 +1200

    Big Update
    I did advanced Player movement as well as torch. I also sorted out building textures. However torch and building textures may need to be tweaked. I need to fix the upper part of my level too. Overall its looking good so far.

--- WalkingSim/CharacterBody3D.gd
+++ WalkingSim/CharacterBody3D.gd
@@ -1,7 +1,10 @@
 extends CharacterBody3D
 
 @export var mouse_sensitivity = 0.001
-const SPEED = 2
+var SPEED = 5.0
+@export var run_speed = 2
+@export var walk_speed = 1
+@export var FRICTION = 0.01
 const JUMP_VELOCITY = 2
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -14,9 +17,15 @@ func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(-event.relative.x * mouse_sensitivity)
 		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
-		$Camera3D.rotation.x = clamp($Camera3D.rotation.x, -1.2,1.2)
+		$Camera3D.rotation.x = clamp($Camera3D.rotation.x, -1.2, 1.2)
 
 func _physics_process(delta):
+	
+	if Input.is_action_pressed("run"):
+		SPEED = run_speed
+	else:
+		SPEED = walk_speed
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -26,12 +35,12 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	# Replace UI actions with custom gameplay actions for WASD movement along with arrows.
+	var input_dir = Input.get_vector("ui_a", "ui_d", "ui_w", "ui_s")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = move_toward(velocity.x, direction.x * SPEED, FRICTION)
+		velocity.z = move_toward(velocity.z, direction.z * SPEED, FRICTION)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
