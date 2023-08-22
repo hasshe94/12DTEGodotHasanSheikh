c46f6cf1970109471dfb00849f0884c07bff090c
hasshe94 <hasshe94@kingshigh.school.nz>
2023-08-22 21:46:13 +1200

    Started Enemy
    Pause menu still needs work. Enemy is glitching so therefore it also needs work.

---
+++ WalkingSim/enemy.gd
@@ -0,0 +1,26 @@
+extends CharacterBody3D
+
+@onready var nav_agent = $NavigationAgent3D
+@onready var player = get_node("/root/World1/Player1")
+
+var movement_speed = 1
+
+func _ready():
+	call_deferred("get_target")
+	
+func get_target():
+	nav_agent.target_position = player.global_transform.origin
+	movement_speed *=1.05
+	
+func _physics_process(delta):
+	if nav_agent.is_navigation_finished():
+		get_target()
+	print(movement_speed)
+	
+	var current_position = global_position
+	var next_position = nav_agent.get_next_path_position()
+	var new_velocity = next_position.normalized() * movement_speed
+	
+	
+	velocity = new_velocity
+	move_and_slide()
